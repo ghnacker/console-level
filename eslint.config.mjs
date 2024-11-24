4899d1e7434792c84740af3ea40343e45d6d0536
Satoshi Nakagawa <ghnacker@gmail.com>
2024-11-24 17:45:06 +0900

    format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,45 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import js from '@eslint/js';
+import {FlatCompat} from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default [
-    {
-        ignores: ["dist/"],
+  {
+    ignores: ['dist/'],
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
     },
-    ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
-    {
-        plugins: {
-            "@typescript-eslint": typescriptEslint,
-        },
 
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-                Atomics: "readonly",
-                SharedArrayBuffer: "readonly",
-            },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+      },
 
-            parser: tsParser,
-            ecmaVersion: 2018,
-            sourceType: "module",
-        },
-
-        rules: {},
+      parser: tsParser,
+      ecmaVersion: 2018,
+      sourceType: 'module',
     },
+
+    rules: {},
+  },
 ];
