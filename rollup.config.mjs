f35b73ec99b41a68845b7e11e47c805f8501e145
Satoshi Nakagawa <ghnacker@gmail.com>
2024-12-28 17:07:27 +0900

    nits

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 
-// const plugins = [typescript(), terser()];
-
 export default [
   {
     input: 'src/index.ts',
