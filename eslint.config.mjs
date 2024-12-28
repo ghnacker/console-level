7bdd26fa9f556a26f776cc8348987c0219f82f9e
Satoshi Nakagawa <ghnacker@gmail.com>
2024-12-28 17:19:55 +0900

    typescript-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,45 +1,12 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import {fileURLToPath} from 'node:url';
-import js from '@eslint/js';
-import {FlatCompat} from '@eslint/eslintrc';
+// @ts-check
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
   {
     ignores: ['dist/'],
   },
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
-  ),
-  {
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-    },
-
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-      },
-
-      parser: tsParser,
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-
-    rules: {},
-  },
-];
+  eslint.configs.recommended,
+  tseslint.configs.recommended
+);
